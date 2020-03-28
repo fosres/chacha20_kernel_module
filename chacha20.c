4d9843c9f38ffcf1f05358d8c5796e241b2e0808
Tanveer Salim <Tanveer.Salim@protonmail.com>
2020-03-28 13:36:49 -0500

    Added prototype for chacha20_state_init

--- chacha20.c
+++ chacha20.c
@@ -60,20 +60,92 @@ void quarteround(unsigned int *a,unsigned int *b,unsigned int *c,unsigned int *d
 	*c += *d; *b ^= *c; *b = rotate_left(*b,7);
 }
 
+void chacha20_state_init(unsigned int * state,unsigned int * key,unsigned int * nonce,unsigned int block)	{
+#if 0	
+	get_random_bytes(key,sizeof(unsigned int)*8);
+
+	get_random_bytes(nonce,sizeof(unsigned int)*3);
+#endif
+	state[0] = 0x61707865;
+
+	state[1] = 0x3320646e;
+
+	state[2] = 0x79622d32;
+
+	state[3] = 0x6b206574;
+
+	unsigned long long int i = 4;
+
+	while ( i < 12 )	{
+		
+		state[i] = reverse(key[i-4]); 
+		
+		i++;
+	}
+	
+// Above while loop incorporates key to state in little endian form
+
+	state[12] = block;
+	
+	i++;
+
+	while ( i < 16 )	{
+		
+		state[i] = reverse(nonce[i-13]);
+
+		i++;
+	}
+}
+
 static int __init chacha20_init(void)	{
 	
-	unsigned int a = 0x11111111;
+	unsigned int * key = (unsigned int*)kzalloc(sizeof(unsigned int)*8,GFP_KERNEL);
 
-	unsigned int b = 0x01020304;
+	unsigned long long int i = 0;
 
-	unsigned int c = 0x9b8d6f43;
-
-	unsigned int d = 0x01234567;
-
-	quarteround(&a,&b,&c,&d);
-
-	printk(KERN_ALERT "%.4x %.4x %.4x %.4x",a,b,c,d);	
+	unsigned long long int dif = 0x04050607 - 0x00010203;
 	
+	key[0]= 0x00010203;
+
+	i++;
+
+	while ( i < 16 	)	{
+		
+		key[i] = key[i-1] + dif;
+
+		i++;
+	}
+	
+	unsigned int * nonce = (unsigned int*)kzalloc(sizeof(unsigned int)*3,GFP_KERNEL);
+
+	i = 0;
+
+	nonce[0] = 0x00000009;
+
+	nonce[1] = 0x0000004a;
+
+	nonce[2] = 0x00000000;
+
+	unsigned int * state = (unsigned int*)kzalloc(sizeof(unsigned int)*16,GFP_KERNEL);
+
+	chacha20_state_init(state,key,nonce,1);
+
+	i = 0;
+
+	while ( i < 16 )	{
+		
+		printk(KERN_ALERT "%.4x",state[i]);
+
+		i++;
+
+	}
+	
+	kfree(state);
+
+	kfree(key);
+
+	kfree(nonce);
+
 	return 0;
 }
 
